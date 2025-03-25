a9c333b19c21a1f019c87f6d0080e9be9dac98b5
nayounsang <nayounsang722@gmail.com>
2025-03-26 00:08:39 +0900

    feat: title ui 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./index.css";
 import { Table } from "./components/Table/Table";
-
+import { AddButton } from "./components/Button/AddButton";
+import styles from "./App.module.css";
 function App() {
   return (
-    <Table />
+    <main>
+      <header className={styles.header}>
+        <h1>회원 목록</h1>
+        <AddButton />
+      </header>
+      <Table />
+    </main>
   );
 }
 
