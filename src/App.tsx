06247cd05826609fd8dcc6f228ff0e7608b975ed
nayounsang <nayounsang722@gmail.com>
2025-03-25 18:36:15 +0900

    feat: 필터 트리거 ui 개발

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { PlusOutlined } from "@ant-design/icons";
 import { Button } from "./components/Button/Button";
 import { MenuButton } from "./components/Button/MenuButton";
 import TextArea from "antd/es/input/TextArea";
-import { DatePicker } from "antd";
+import { Checkbox, DatePicker } from "antd";
 import { Select } from "./components/Select";
+import { FilterTrigger } from "./components/Filter/FilterTrigger";
 
 function App() {
   return (
@@ -22,6 +23,8 @@ function App() {
           { label: "디자이너", value: "디자이너" },
         ]}
       />
+      <Checkbox/>
+      <FilterTrigger label="필터" />
     </>
   );
 }
