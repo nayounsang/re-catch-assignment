48493f1d59312d677fd3b7649735e299703cfb70
nayounsang <nayounsang722@gmail.com>
2025-03-26 16:10:09 +0900

    feat: pretendard 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import "./index.css";
 import { Table } from "./components/Table/Table";
 import { AddButton } from "./components/Button/AddButton";
 import styles from "./App.module.css";
+import { ConfigProvider } from "antd";
 function App() {
   return (
-    <main>
-      <header className={styles.header}>
-        <h1>회원 목록</h1>
-        <AddButton />
-      </header>
-      <Table />
-    </main>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily:
+            'Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", sans-serif',
+        },
+      }}
+    >
+      <main>
+        <header className={styles.header}>
+          <h1>회원 목록</h1>
+          <AddButton />
+        </header>
+        <Table />
+      </main>
+    </ConfigProvider>
   );
 }
 
